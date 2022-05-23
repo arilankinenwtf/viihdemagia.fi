e3e260047c5a7814b5ec438793ac92a722d5424e
telekkariarska <arskathestroyer@gmail.com>
2022-05-23 19:47:20 +0300

    feature updates

--- www/application/themes/viihdeteema/elements/containers/kuva_oikea.php
+++ www/application/themes/viihdeteema/elements/containers/kuva_oikea.php
@@ -5,14 +5,14 @@ use Concrete\Core\Area\ContainerArea;
 ?>
 <div class="container custom-container kuva-container kuva-oikea">
     <div class="row">
-        <div class="col-12 col-md-6 feature-vasen">
+        <div class="col-12 col-md-7 feature-vasen">
             <?php
             $area = new ContainerArea($container, 'Feature/esittely tähän');
             $a->setAreaGridMaximumColumns(12);
             $area->display($c);
             ?>
         </div>
-        <div class="col-12 col-md-6 kuva-right">
+        <div class="col-12 col-md-5 kuva-right">
             <?php
             $area = new ContainerArea($container, 'Kuva tähän');
             $a->setAreaGridMaximumColumns(12);
--- www/application/themes/viihdeteema/elements/containers/kuva_vasen.php
+++ www/application/themes/viihdeteema/elements/containers/kuva_vasen.php
@@ -5,14 +5,14 @@ use Concrete\Core\Area\ContainerArea;
 ?>
 <div class="container custom-container kuva-container kuva-vasen">
     <div class="row">
-        <div class="col-12 col-md-6">
+        <div class="col-12 col-md-5">
             <?php
             $area = new ContainerArea($container, 'kuva tähän');
             $a->setAreaGridMaximumColumns(12);
             $area->display($c);
             ?>
         </div>
-        <div class="col-12 col-md-6 kuva-left">
+        <div class="col-12 col-md-7 kuva-left">
             <?php
             $area = new ContainerArea($container, 'feature/esittely tähän');
             $a->setAreaGridMaximumColumns(12);
