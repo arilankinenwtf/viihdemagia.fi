1feb1ecb36f1cde89a903e13c422d70e2e37edad
Ari <arilankinen4@gmail.com>
2022-06-07 22:17:58 +0300

    js change

--- www/application/blocks/feature/view.js
+++ www/application/blocks/feature/view.js
@@ -44,7 +44,7 @@ $(document).ready(function () {
       }
     });
   } else {
-    $('.feature-toggle').on('click', function (e) {
+    $('.feature-toggle-wrapper').on('click', function (e) {
       e.preventDefault();
   
       if($(this).hasClass('active')) {
@@ -56,7 +56,31 @@ $(document).ready(function () {
       }
     });
   
-    $('.feature-toggle').on('click', function (e) {
+    $('.feature-toggle-wrapper').on('click', function (e) {
+      if($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $(this).parent().find('.feature-toggle-open').collapse('toggle');
+        $(this).parent().find('a').removeClass('active').attr('aria-expanded','false');
+      } else {
+        $(this).addClass('active');
+        $(this).parent().find('.feature-toggle-open').collapse('toggle');
+        $(this).parent().find('a').addClass('active').attr('aria-expanded','true');
+      }
+    });
+
+    $('.feature-toggle-icon').on('click', function (e) {
+      e.preventDefault();
+  
+      if($(this).hasClass('active')) {
+        $(this).removeClass('active').attr('aria-expanded','false');
+        $(this).parent().find('.feature-toggle-icon').removeClass('active');
+      } else {
+        $(this).addClass('active').attr('aria-expanded','true');
+        $(this).parent().find('.feature-toggle-icon').addClass('active');
+      }
+    });
+  
+    $('.feature-toggle-icon').on('click', function (e) {
       if($(this).hasClass('active')) {
         $(this).removeClass('active');
         $(this).parent().find('.feature-toggle-open').collapse('toggle');
