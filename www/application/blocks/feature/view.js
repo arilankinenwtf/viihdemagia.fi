2639d7aa63a08771cf4020dcd6a337936e92eeb0
Ari <arilankinen4@gmail.com>
2022-06-07 22:26:21 +0300

    js change

--- www/application/blocks/feature/view.js
+++ www/application/blocks/feature/view.js
@@ -44,30 +44,6 @@ $(document).ready(function () {
       }
     });
   } else {
-    $('.feature-toggle-wrapper').on('click', function (e) {
-      e.preventDefault();
-  
-      if($(this).hasClass('active')) {
-        $(this).removeClass('active').attr('aria-expanded','false');
-        $(this).parent().find('.feature-toggle-icon').removeClass('active');
-      } else {
-        $(this).addClass('active').attr('aria-expanded','true');
-        $(this).parent().find('.feature-toggle-icon').addClass('active');
-      }
-    });
-  
-    $('.feature-toggle-wrapper').on('click', function (e) {
-      if($(this).hasClass('active')) {
-        $(this).removeClass('active');
-        $(this).parent().find('.feature-toggle-open').collapse('toggle');
-        $(this).parent().find('a').removeClass('active').attr('aria-expanded','false');
-      } else {
-        $(this).addClass('active');
-        $(this).parent().find('.feature-toggle-open').collapse('toggle');
-        $(this).parent().find('a').addClass('active').attr('aria-expanded','true');
-      }
-    });
-
     $('.feature-toggle-icon').on('click', function (e) {
       e.preventDefault();
   
