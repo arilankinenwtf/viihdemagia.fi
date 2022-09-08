d05bffa677fade91e56eba4b71a63964217dcd30
Ari <arilankinen4@gmail.com>
2022-09-08 12:25:31 +0300

    news fix

--- www/application/blocks/page_list/templates/news_list/view.php
+++ www/application/blocks/page_list/templates/news_list/view.php
@@ -19,24 +19,25 @@ $c = Page::getCurrentPage();
       if ($useButtonForLink) {
         $hoverLinkText = $buttonLinkText;
       }
-      if($page->getCollectionDatePublic()) {
-        $date = $page->getCollectionDatePublicObject()->format('j.n.Y');
-      }
+
+      $date = '';
+      if($page->getCollectionDatePublic()) { $date = $page->getCollectionDatePublicObject()->format('j.n.Y');}
+
+      $src = '';
       if($page->getAttribute('main_image')) {
         $type = \Concrete\Core\File\Image\Thumbnail\Type\Type::getByHandle('news_image_thumbnail');
         $main_image = $page->getAttribute('main_image');
-        $src = $main_image->getThumbnailURL($type->getBaseVersion());
-      } else {
-        $src = '';
       }
       ?>
     <div class="news-list-item-content">
+      <a href="<?php echo $url ?>" target="<?php echo $target; ?>">
+        <img class="news-list-item-thumbnail" src="<?php echo $src; ?>" alt="<?php echo $title ?>">
+      </a>
+
       <div class="news-list-item-text-wrapper">
         <div class="news-list-item-date"><?php echo $date; ?></div>
         <h2 class="news-list-item-title">
-        <a href="<?php echo $url ?>" target="<?php echo $target; ?>">
           <?php echo $title ?>
-        </a>
         </h2>
 
         <?php if ($includeDescription): ?>
@@ -47,7 +48,7 @@ $c = Page::getCurrentPage();
 
         <?php if (isset($useButtonForLink) && $useButtonForLink): ?>
         <div class="news-list-item-readmore top-border">
-            <a href="<?php echo $url?>" target="<?php echo $target?>" class="btn btn-secondary <?php echo $buttonClasses?>"><?php echo $buttonLinkText?></a>
+            <a href="<?php echo $url?>" target="<?php echo $target?>" class="btn btn-secondary"><?php echo $buttonLinkText?></a>
         </div>
         <?php endif; ?>
       </div>
