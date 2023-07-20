4e247bb45e77ec8a72b2580067eb7b0ed29d811d
Ari <arilankinen4@gmail.com>
2023-07-20 17:58:46 +0300

    news image php fix

--- www/application/blocks/page_list/templates/news_list/view.php
+++ www/application/blocks/page_list/templates/news_list/view.php
@@ -23,35 +23,38 @@ $c = Page::getCurrentPage();
       $date = '';
       if($page->getCollectionDatePublic()) { $date = $page->getCollectionDatePublicObject()->format('j.n.Y');}
 
-      $main_image = '';
-      if($c->getAttribute('main_image')) {
-        $main_image = $c->getAttribute('main_image');
-        $main_image = $main_image->getURL(); 
+      $src = '';
+      if($page->getAttribute('main_image')) {
+        $type = \Concrete\Core\File\Image\Thumbnail\Type\Type::getByHandle('news_image_thumbnail');
+        $main_image = $page->getAttribute('main_image');
+        $src = $main_image->getThumbnailURL($type->getBaseVersion());
+        $poistettava = '/thumbnails/news_image_thumbnail';
+        $uusiUrl = str_replace($poistettava, '', $src);
       }
       ?>
     <div class="news-list-item-content">
       <a href="<?php echo $url ?>" target="<?php echo $target; ?>">
-        <img class="news-list-item-thumbnail" src="<?php echo $main_image; ?>" alt="<?php echo $title ?>">
+        <img class="news-list-item-thumbnail" src="<?php echo $uusiUrl; ?>" alt="<?php echo $title ?>">
 
-        <div class="news-list-item-text-wrapper">
-          <div class="news-list-item-date"><?php echo $date; ?></div>
-          <h2 class="news-list-item-title">
-            <?php echo $title ?>
-          </h2>
-
-          <?php if ($includeDescription): ?>
-          <div class="news-list-item-description">
-            <?php echo $description ?>
-          </div>
-          <?php endif; ?>
-
-          <?php if (isset($useButtonForLink) && $useButtonForLink): ?>
-          <div class="news-list-item-readmore top-border">
-            <a href="<?php echo $url?>" target="<?php echo $target?>" class="btn btn-secondary"><?php echo $buttonLinkText?></a>
-          </div>
-          <?php endif; ?>
-        </div>
+      <div class="news-list-item-text-wrapper">
+        <div class="news-list-item-date"><?php echo $date; ?></div>
+        <h2 class="news-list-item-title">
+          <?php echo $title ?>
+        </h2>
       </a>
+
+        <?php if ($includeDescription): ?>
+        <div class="news-list-item-description">
+          <?php echo $description ?>
+        </div>
+        <?php endif; ?>
+
+        <?php if (isset($useButtonForLink) && $useButtonForLink): ?>
+        <div class="news-list-item-readmore top-border">
+            <a href="<?php echo $url?>" target="<?php echo $target?>" class="btn btn-secondary"><?php echo $buttonLinkText?></a>
+        </div>
+        <?php endif; ?>
+      </div>
     </div>
     
   </div>
